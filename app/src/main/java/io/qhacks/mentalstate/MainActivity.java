14ab23ee7dcbbc57b620e9451de6d4c4d0d5798d
codyweeden <cw_13@live.ca>
2017-02-04 18:04:02 -0500

    graphing should work

--- app/src/main/java/io/qhacks/mentalstate/MainActivity.java
+++ app/src/main/java/io/qhacks/mentalstate/MainActivity.java
@@ -2,16 +2,13 @@ package io.qhacks.mentalstate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
 
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -39,16 +36,80 @@ public class MainActivity extends AppCompatActivity {
 //        String[] myDataset = {"Notifications"};
 //        mAdapter = new MyAdapter(myDataset);
 //        mRecyclerView.setAdapter(mAdapter);
+        try {
+            FileInputStream fstream = new FileInputStream("data.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine = "";
+            int i= 0;
+            //String[] arrStr = new String [i];
+
+            while ((strLine = br.readLine()) != null) {
+                i++;
+            }
 
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 1),
-                new DataPoint(2, 0)
-        });
-        graph.addSeries(series);
-    }
+            FileInputStream fstream2 = new FileInputStream("data.txt");
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(fstream2));
+            String[] arrStr = new String [i];
+
+            while ((strLine = br2.readLine()) != null) {
+                arrStr[i] = strLine;
+                i++;
+            }
+
+
+            FileInputStream fstream3 = new FileInputStream("data.txt");
+            BufferedReader br3 = new BufferedReader(new InputStreamReader(fstream3));
+            final int j = i - 14;
+
+            int a[]=new int[14];
+            int b[]=new int[14];
+            while(i>j )
+
+            {
+                String dat = arrStr[i];
+                if (arrStr[i] != null) {
+                    a[i] = Integer.parseInt(Character.toString(dat.charAt(0)));
+                    b[i] = Integer.parseInt(Character.toString(dat.charAt(2)));
+                }
+                else{
+                    a[i] = 0;
+                    b[i] = 0;
+                }
+                i--;
+
+            }
+            GraphView graph = (GraphView) findViewById(R.id.graph);
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(a[1],b[1]),
+                new DataPoint(a[2],b[2]),
+                new DataPoint(a[3],b[3]),
+                new DataPoint(a[4],b[4]),
+                new DataPoint(a[5],b[5]),
+                new DataPoint(a[6],b[6]),
+                new DataPoint(a[7],b[7]),
+                new DataPoint(a[8],b[8]),
+                new DataPoint(a[9],b[9]),
+                new DataPoint(a[10],b[10]),
+                new DataPoint(a[11],b[11]),
+                new DataPoint(a[12],b[12]),
+                new DataPoint(a[13],b[13]),
+                new DataPoint(a[14],b[14])
+            });
+            graph.addSeries(series);
+
+//Read File Line By Line
+        }
+        catch (java.io.FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (java.io.IOException e){
+            e.printStackTrace();
+        }
+
+
+}
+
 }
 
 
