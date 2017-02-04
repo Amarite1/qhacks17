7c5886e2ffd2219a7b9f7921ec90f69737f3fd47
codyweeden <cw_13@live.ca>
2017-02-04 13:59:56 -0500

    finally got charts working

--- app/src/main/java/io/qhacks/mentalstate/MainActivity.java
+++ app/src/main/java/io/qhacks/mentalstate/MainActivity.java
@@ -12,6 +12,10 @@ import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -35,62 +39,16 @@ public class MainActivity extends AppCompatActivity {
         String[] myDataset = {"Notifications"};
         mAdapter = new MyAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
-        WebView webview = (WebView) findViewById(R.id.webView1);
-        String content = "<html>"
-                + "  <head>"
-                + "    <script type=\"text/javascript\" src=\"jsapi.js\"></script>"
-                + "    <script type=\"text/javascript\">"
-                + "      google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});"
-                + "      google.setOnLoadCallback(drawChart);"
-                + "      function drawChart() {"
-                + "        var data = google.visualization.arrayToDataTable(["
-                + "          ['Year', 'Sales', 'Expenses'],"
-                + "          ['2010',  1000,      400],"
-                + "          ['2011',  1170,      460],"
-                + "          ['2012',  660,       1120],"
-                + "          ['2013',  1030,      540]"
-                + "        ]);"
-                + "        var options = {"
-                + "          title: 'Truiton Performance',"
-                + "          hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}"
-                + "        };"
-                + "        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));"
-                + "        chart.draw(data, options);"
-                + "      }"
-                + "    </script>"
-                + "  </head>"
-                + "  <body>"
-                + "    <div id=\"chart_div\" style=\"width: 1000px; height: 500px;\"></div>"
-                + "    <img style=\"padding: 0; margin: 0 0 0 330px; display: block;\" src=\"truiton.png\"/>"
-                + "  </body>" + "</html>";
 
-        WebSettings webSettings = webview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webview.requestFocusFromTouch();
-        webview.loadDataWithBaseURL( "file:///android_asset/", content, "text/html", "utf-8", null );
-        //webview.loadUrl("file:///android_asset/Code.html"); // Can be used in this way too.
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 1),
+                new DataPoint(2, 0)
+        });
+        graph.addSeries(series);
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.action_to_image:
-                Intent intent = new Intent(MainActivity.this, GoogleImageGraphActivity.class);
-                startActivity(intent);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }*/
 }
 
 
