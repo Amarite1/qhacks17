bf9b87c39f5203fbc194e0ba3f5243a3e38d59b0
jessi369 <15jfd@queensu.ca>
2017-02-04 04:03:10 -0500

    update

--- app/src/main/java/io/qhacks/mentalstate/LocationFinder.java
+++ app/src/main/java/io/qhacks/mentalstate/LocationFinder.java
@@ -95,4 +95,17 @@ public class MyActivity extends Activity
         mResolvingError = savedInstanceState != null
                 && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
     }
+    public class MainActivity extends ActionBarActivity implements
+            ConnectionCallbacks, OnConnectionFailedListener {
+        ...
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+            if (mLastLocation != null) {
+                mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+                mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            }
+        }
+    }
 }
