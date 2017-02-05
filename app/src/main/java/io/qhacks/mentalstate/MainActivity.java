116856ad3b6cbdefb57cccac9dbdb16a6de30ea6
Ben W <me@t3kbau5.com>
2017-02-04 22:34:19 -0500

    Main Screen Buttons These work now

--- app/src/main/java/io/qhacks/mentalstate/MainActivity.java
+++ app/src/main/java/io/qhacks/mentalstate/MainActivity.java
@@ -1,5 +1,8 @@
 package io.qhacks.mentalstate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,8 +12,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private Context _this = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +46,11 @@ public class MainActivity extends AppCompatActivity {
 //        String[] myDataset = {"Notifications"};
 //        mAdapter = new MyAdapter(myDataset);
 //        mRecyclerView.setAdapter(mAdapter);
-
-       DailyPollManager.enableNotifications(this, 21);
-
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!prefs.getBoolean("enabled", false)) {
+            DailyPollManager.enableNotifications(this, 21);
+            prefs.edit().putBoolean("enabled", true);
+        }
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
@@ -49,6 +58,26 @@ public class MainActivity extends AppCompatActivity {
                 new DataPoint(2, 0)
         });
         graph.addSeries(series);
+
+        Button goodButton = (Button) findViewById(R.id.goodbutton);
+        goodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_this, DailyPollResponseReceiver.class);
+                intent.putExtra("type", 1);
+                sendBroadcast(intent);
+            }
+        });
+
+        Button badButton = (Button) findViewById(R.id.button4);
+        badButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_this, DailyPollResponseReceiver.class);
+                intent.putExtra("type", 0);
+                sendBroadcast(intent);
+            }
+        });
     }
 }
 
