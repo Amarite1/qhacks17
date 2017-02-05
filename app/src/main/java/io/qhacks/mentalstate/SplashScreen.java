9c191eee274e44532ff44d0415e4a6f3393520d3
jessi369 <15jfd@queensu.ca>
2017-02-04 23:08:21 -0500

    anim

--- app/src/main/java/io/qhacks/mentalstate/SplashScreen.java
+++ app/src/main/java/io/qhacks/mentalstate/SplashScreen.java
@@ -3,6 +3,10 @@ package io.qhacks.mentalstate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Allan on 2017-02-04.
@@ -14,7 +18,7 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-
+RunAnimation();
         Thread timerThread = new Thread(){
             public void run(){
                 try {
@@ -34,6 +38,16 @@ public class SplashScreen extends Activity {
     protected void onPause() {
         super.onPause();
         finish();
+    }
+
+    private void RunAnimation()
+    {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.anim1);
+        a.reset();
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        tv.clearAnimation();
+        tv.startAnimation(a);
+
     }
 
 }
